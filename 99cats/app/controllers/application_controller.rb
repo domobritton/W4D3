959ce1bab8ff25b0019c398ca3bab861d603eb92
domonic <domonicbritton@gmail.com>
2018-09-12 15:00:56 -0700

    Create controllers and routes for users and sessions

--- 99cats/app/controllers/application_controller.rb
+++ 99cats/app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  
+  def current_user
+    @current_user ||= User.find_by(session_token: session[session_token])
+  end
+  
+  def login(user)
+    @current_user = user
+    session[session_token] = User.reset_session_token!
+  end
+  
+  def require_login
+    redirect_to new_session_url unless logged_in?
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+  
+  def require_logout
+    redirect_to users_url if logged_in?
+  end
+  
+  def logout!
+    current_user.reset_session_token!
+    session[session_token] = nil
+    current_user = nil
+  end
 end
--- 99cats/db/migrate/20180912191251_create_users.rb
+++ 99cats/db/migrate/20180912191251_create_users.rb
@@ -9,6 +9,5 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     end
     add_index :users, :user_name, unique: true 
     add_index :users, :session_token, unique: true 
-    
   end
 end
